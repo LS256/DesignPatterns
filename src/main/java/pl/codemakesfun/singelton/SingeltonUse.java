02aca5c6224eb6f05df5d63d773178effa835742
LS256 <lstaszew@gmail.com>
2018-04-10 23:12:33 +0200

    Lets check how git is working under Eclipse

--- src/main/java/pl/codemakesfun/singelton/SingeltonUse.java
+++ src/main/java/pl/codemakesfun/singelton/SingeltonUse.java
@@ -1,5 +1,9 @@
 package pl.codemakesfun.singelton;
 
+/*
+ * Class where Singelton pattern is presented.
+ * Singelton means.. There can be only one
+ */
 public class SingeltonUse {
 
     public static void main(String[] args) {
